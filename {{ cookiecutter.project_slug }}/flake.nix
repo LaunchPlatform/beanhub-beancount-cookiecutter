e56185c291d42e0a49a22ea9863b74a6c5d09ccc
Fang-Pen Lin <hello@fangpenlin.com>
2024-05-24 20:57:19 -0700

    Add nix flake

---
+++ {{ cookiecutter.project_slug }}/flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+    flake-utils = {
+      url = "github:numtide/flake-utils/v1.0.0";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ attrs:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        formatter = pkgs.alejandra;
+        devShells.default = pkgs.mkShell {
+          env = {
+            LC_ALL = "C.UTF-8";
+          };
+          packages = with pkgs; [
+            beanhub-cli
+            beancount
+          ];
+        };
+      }
+    );
+}
